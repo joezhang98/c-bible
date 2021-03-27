fccfc4346091f87b4c13ec81a0d8c277b10a55c5
Zhao Zhang <zz98@stanford.edu>
2021-03-27 03:53:28 +0000

    finished reading chapter 1

--- chapter1/fahr2celsius.c
+++ chapter1/fahr2celsius.c
@@ -4,6 +4,8 @@
 #define UPPER 300     /* upper limit */
 #define STEP  20      /* step size */
 
+float fahr2celsius(int fahr);
+
 /* print Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300; floating-point version */
 int main()
@@ -13,7 +15,13 @@ int main()
   printf("Fahrenheit to Celsius conversion:\n");
 
   for (fahr = 300; fahr >= 0; fahr = fahr - 20) {
-    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32.0));
+    printf("%3d %6.1f\n", fahr, fahr2celsius(fahr));
   }
+}
+
+/* convert Fahrenheit to Celsius */
+float fahr2celsius(int fahr)
+{
+  return (5.0/9.0) * (fahr-32.0);
 }
 
